8c40a7951f5d4261d829ea25eea004d4b2e8949b
czoido <mrgalleta@gmail.com>
2024-10-21 12:08:42 +0200

    simplify example

--- lesson1/src/main.cpp
+++ lesson1/src/main.cpp
@@ -2,10 +2,7 @@
 #include <fmt/color.h>
 
 int main() {
-    fmt::print(fmt::fg(fmt::color::cyan) | fmt::emphasis::bold, "Conan is a ");
-    fmt::print(fmt::fg(fmt::color::yellow) | fmt::emphasis::bold, "MIT-licensed, ");
-    fmt::print(fmt::fg(fmt::color::green) | fmt::emphasis::bold, "Open Source ");
-    fmt::print(fmt::fg(fmt::color::white), "package manager for ");
-    fmt::print(fmt::fg(fmt::color::magenta) | fmt::emphasis::bold, "C and C++ development\n");
+    fmt::print(fmt::fg(fmt::color::cyan) | fmt::emphasis::bold, "Conan is a MIT-licensed, Open Source ");
+    fmt::print(fmt::fg(fmt::color::white), "package manager for C and C++ development\n");
     return 0;
 }
