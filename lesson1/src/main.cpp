50e795b055f5d76fa931c77241012097054d76bc
czoido <mrgalleta@gmail.com>
2024-10-21 15:31:21 +0200

    wip

--- lesson1/src/main.cpp
+++ lesson1/src/main.cpp
@@ -2,7 +2,9 @@
 #include <fmt/color.h>
 
 int main() {
-    fmt::print(fmt::fg(fmt::color::cyan) | fmt::emphasis::bold, "Conan is a MIT-licensed, Open Source ");
-    fmt::print(fmt::fg(fmt::color::white), "package manager for C and C++ development\n");
+    fmt::print(fmt::fg(fmt::color::cyan) | fmt::emphasis::bold, 
+               "Conan is a MIT-licensed, Open Source ");
+    fmt::print(fmt::fg(fmt::color::white), 
+               "package manager for C and C++ development\n");
     return 0;
 }
