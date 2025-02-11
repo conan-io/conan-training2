388446bedf1d390c19a31917a067ef7d6f252da3
Abril Rincón Blanco <abrilrb@jfrog.com>
2025-02-11 16:13:50 +0100

    Too much

--- lesson3/src/main.cpp
+++ lesson3/src/main.cpp
@@ -11,13 +11,16 @@ int main() {
 #ifdef USE_TABULATE
     Table info;
     info.add_row({
-        "Conan is an Open Source package manager"
+        "Conan is MIT-licensed",
+        "Open Source package manager",
     });
     info[0][0].format().font_color(Color::green).font_style({FontStyle::bold});
     std::cout << info << std::endl;
 #else
     fmt::print(fmt::fg(fmt::color::cyan) | fmt::emphasis::bold, 
-               "Conan is an Open Source package manager\n");
+               "Conan is a MIT-licensed, Open Source ");
+    fmt::print(fmt::fg(fmt::color::white),
+               "package manager for C and C++ development\n");
 #endif
 
     return 0;
