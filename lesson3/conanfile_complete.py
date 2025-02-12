510f84d7c520cdb87476025f2d23975890acb25d
Abril Rincón Blanco <abrilrb@jfrog.com>
2025-02-12 13:01:15 +0100

    New complete conanfile

--- lesson3/conanfile_complete.py
+++ lesson3/conanfile_complete.py
@@ -1,5 +1,6 @@
 from conan import ConanFile
 from conan.tools.cmake import cmake_layout, CMakeToolchain, CMakeDeps
+from conan.tools.build import valid_min_cppstd
 from conan.errors import ConanInvalidConfiguration
 
 
@@ -9,11 +10,10 @@ class FormatterRecipe(ConanFile):
     options = {"with_tabulate": [True, False]}
     default_options = {"with_tabulate": False}
 
-    def requirements(self):
-        if self.options.with_tabulate:
-            self.requires("tabulate/1.5")
-        else:
-            self.requires("fmt/11.0.2")
+    def validate(self):
+        if (self.options.with_tabulate and
+                valid_min_cppstd(self, 17)):
+            raise ConanInvalidConfiguration("Tabulate requires C++17")
 
     def generate(self):
         tc = CMakeToolchain(self)
@@ -23,9 +23,12 @@ class FormatterRecipe(ConanFile):
         cd = CMakeDeps(self)
         cd.generate()
 
-    def validate(self):
-        if self.options.with_tabulate and self.settings.build_type == "Debug":
-            raise ConanInvalidConfiguration("Tabulate is not supported in Debug mode.")
+    def requirements(self):
+        if self.options.with_tabulate:
+            self.requires("tabulate/1.5")
+        else:
+            self.requires("fmt/11.0.2")
+
 
     def layout(self):
         cmake_layout(self)
