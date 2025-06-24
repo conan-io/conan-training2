7aac8b923a4b4d9e4b31aa5fff3257468bd42475
Carlos Zoido <mrgalleta@gmail.com>
2025-06-24 06:34:45 +0200

    add lesson16

---
+++ lesson16/test_package/conanfile.py
@@ -0,0 +1,20 @@
+from conan import ConanFile
+
+
+class secure_scannerTestConan(ConanFile):
+    settings = "os", "compiler", "build_type", "arch"
+
+    def build_requirements(self):
+        self.tool_requires(self.tested_reference_str)
+
+    def build(self):
+        var_cmd = (
+            "echo MY_VAR=%MY_VAR%"
+            if self.settings_build.os == "Windows"
+            else "echo MY_VAR=$MY_VAR"
+        )
+        self.run(var_cmd)
+
+    def test(self):
+        extension = ".exe" if self.settings_build.os == "Windows" else ""
+        self.run("secure_scanner{} mypath".format(extension))
