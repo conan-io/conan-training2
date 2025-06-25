1b38dc3f49706ceb006143855e1e4b717b0883ac
Daniel <danimanzaneque@gmail.com>
2025-06-25 15:18:20 +0200

    [WIP] Add lesson 15 materials (#15)
    * Add lesson15 materials
    
    * add changes for new structure
    
    * original
    
    * reduce vendor directories
    
    * update materials - review ernesto
    
    * remove line
    
    * Update .gitignore
    
    * Update lesson15/development/mylib/src/mylib.cpp
    
    ---------
    
    Co-authored-by: Carlos Zoido <mrgalleta@gmail.com>

---
+++ lesson15/prebuilt/conanfile.py
@@ -0,0 +1,25 @@
+import os
+from conan import ConanFile
+from conan.tools.files import copy
+
+
+class helloRecipe(ConanFile):
+    name = "hello"
+    version = "0.1"
+    settings = "os", "arch"
+
+    def layout(self):
+        _os = str(self.settings.os).lower()
+        _arch = str(self.settings.arch).lower()
+        self.folders.build = os.path.join("vendor_hello_library", _os, _arch)
+
+    def package(self):
+        copy(self, "*.h", os.path.join(self.build_folder, "include"),
+            os.path.join(self.package_folder, "include"), keep_path=False)
+        copy(self, "*.lib", self.build_folder,
+            os.path.join(self.package_folder, "lib"), keep_path=False)
+        copy(self, "*.a", self.build_folder,
+            os.path.join(self.package_folder, "lib"), keep_path=False)
+
+    def package_info(self):
+        self.cpp_info.libs = ["hello"]
