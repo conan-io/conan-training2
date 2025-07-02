b8630f8736fced8294e7bb68a515819aa3baa249
Carlos Zoido <mrgalleta@gmail.com>
2025-07-02 14:40:16 +0200

    lesson4

---
+++ lesson4/test.bat
@@ -0,0 +1,21 @@
+@echo off
+
+conan install . --build=missing
+
+REM This version should be the one installed in your system
+cmake --version  
+
+REM By activating the conanbuild environment, the new cmake version will correspond to the one listed in build requirements
+
+REM Windows
+build\Release\generators\conanbuild.bat
+
+cmake --version
+
+cmake --preset=conan-release
+cmake --build --preset=conan-release
+
+REM Windows
+build\Release\generators\conanrun.bat
+
+build\Release\formatter 
---
+++ lesson4/test.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+conan install . --build=missing
+
+# This version should be the one installed in your system
+cmake --version  
+
+# By activating the conanbuild environment, the new cmake version will correspond to the one listed in build requirements
+
+# Linux and macOS
+. build/Release/generators/conanbuild.sh
+
+cmake --version
+
+cmake --preset=conan-release
+cmake --build --preset=conan-release
+
+# Linux and macOS
+. build/Release/generators/conanrun.sh
+
+./build/Release/formatter 
