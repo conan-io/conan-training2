bbac09587205981c78ea6f0ffdc9a6552a0d8317
Carlos Zoido <mrgalleta@gmail.com>
2025-07-02 12:28:09 +0200

    lesson2

---
+++ lesson2/test.bat
@@ -0,0 +1,15 @@
+conan install . --build=missing -pr=debug
+cmake --preset=conan-default
+cmake --build --preset=conan-debug
+build/Debug/formatter
+
+conan install . --build=missing --options="*:shared=True"
+cmake --preset=conan-default
+cmake --build --preset=conan-release
+build/Release/formatter
+
+build/generators/conanrun.bat
+
+build/Release/formatter
+
+conan list "fmt/11.0.2:*" --format=compact
