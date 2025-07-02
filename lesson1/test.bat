cc27914412c6735ac64a8a0c0a6cba66d7f31853
Carlos Zoido <mrgalleta@gmail.com>
2025-07-02 11:48:59 +0200

    basic ci

---
+++ lesson1/test.bat
@@ -0,0 +1,6 @@
+conan profile detect --force
+conan install . --build=missing
+cmake --list-presets
+cmake --preset=conan-default
+cmake --build --preset conan-release
+./build/Release/formatter
---
+++ lesson1/test.sh
@@ -0,0 +1,6 @@
+conan profile detect --force
+conan install . --build=missing
+cmake --list-presets
+cmake --preset=conan-release
+cmake --build --preset=conan-release
+./build/Release/formatter
