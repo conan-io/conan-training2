c509b8eefaf3fbf3f6b01c289aaf7b057d6ddeea
Carlos Zoido <mrgalleta@gmail.com>
2025-07-02 14:38:51 +0200

    wip

--- lesson3/test.bat
+++ lesson3/test.bat
@@ -1,9 +1,8 @@
 @echo off
 
-conan install . --build=missing -o="&:with_std_format=True"
-
 conan install conanfile_complete.py --build=missing -o="&:with_std_format=True" -s="compiler.cppstd=20"
 
 cmake --preset=conan-default
-cmake --build --preset=conan-debug
-build\Release\formatter 
+cmake --build --preset=conan-release
+
+build\Release\formatter
