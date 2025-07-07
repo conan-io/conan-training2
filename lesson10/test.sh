383b6fbb4ac90fba0858fae49213a1581e179b9c
Carlos Zoido <mrgalleta@gmail.com>
2025-07-07 15:33:29 +0200

    wip

--- lesson10/test.sh
+++ lesson10/test.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
 # Check how it fails, then change hellolib for hello in the conanfile.py
-sed -i '' 's/hellolib/hello/g' conanfile.py
+# Detect OS and use appropriate sed command
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    # macOS
+    sed -i '' 's/hellolib/hello/g' conanfile.py
+else
+    # Linux and other Unix systems
+    sed -i 's/hellolib/hello/g' conanfile.py
+fi
 
 # Create the package again, it succeeds
 conan create . 
