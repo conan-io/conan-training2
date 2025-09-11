0133eefeff67738db4c303720c06a0c32065d291
Carlos Zoido <mrgalleta@gmail.com>
2025-09-11 17:03:17 +0200

    wip

--- lesson20/tests/test.cpp
+++ lesson20/tests/test.cpp
@@ -2,11 +2,6 @@
 #include "gtest/gtest.h"
 
 namespace {
-    TEST(HelloTest, ComposeMessagesEmpty) {
-        EXPECT_EQ(std::string("hello/1.0: Hello World ! ()\n"), 
-                  compose_message("", ""));
-    }
-
     TEST(HelloTest, ComposeMessagesSpecialChars) {
         EXPECT_EQ(std::string("hello/1.0: Hello World Special! (with spaces & symbols!)\n"), 
                   compose_message("Special", "with spaces & symbols!"));
