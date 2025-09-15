96a15ac1dc79c578a70471991fca6cbc27a7b830
Carlos Zoido <mrgalleta@gmail.com>
2025-09-15 16:25:06 +0200

    wip

--- lesson19/math/conanfile_library.py
+++ lesson19/math/conanfile_library.py
@@ -17,17 +17,3 @@ class Math(ConanFile):
         "fPIC": True,
         "header_only": False
     }
-
-    # def config_options(self):
-    #     if self.settings.os == "Windows":
-    #         self.options.rm_safe("fPIC")
-
-    # def configure(self):
-    #     if self.options.get_safe("shared") or self.options.header_only:
-    #         self.options.rm_safe("fPIC")
-    #     if self.options.header_only:
-    #         self.options.rm_safe("shared")
-
-    # def package_id(self):
-    #     if self.info.options.header_only:
-    #         self.info.clear()
