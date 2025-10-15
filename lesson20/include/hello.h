695b97f7fd22d1cacf33741006228d4e197ddaec
Carlos Zoido <mrgalleta@gmail.com>
2025-10-15 15:40:22 +0200

    Lesson20 (#25)
    * wip
    
    * wip
    
    * wip

---
+++ lesson20/include/hello.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <vector>
+#include <string>
+
+
+#ifdef _WIN32
+  #define HELLO_EXPORT __declspec(dllexport)
+#else
+  #define HELLO_EXPORT
+#endif
+
+HELLO_EXPORT void hello();
+HELLO_EXPORT void hello_print_vector(const std::vector<std::string> &strings);
+HELLO_EXPORT std::string compose_message(const std::string& build_type, const std::string& extra_info);
---
+++ lesson20/tests/test.cpp
@@ -0,0 +1,9 @@
+#include "../include/hello.h"
+#include "gtest/gtest.h"
+
+namespace {
+    TEST(HelloTest, ComposeMessagesSpecialChars) {
+        EXPECT_EQ(std::string("hello/1.0: Hello World Special! (with spaces & symbols!)\n"), 
+                  compose_message("Special", "with spaces & symbols!"));
+    }
+}
