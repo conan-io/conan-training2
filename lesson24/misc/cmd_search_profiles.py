edcf0efe4865627fe90f8ed9875c9053526a103d
Carlos Zoido <mrgalleta@gmail.com>
2025-10-22 16:45:52 +0200

    wip

--- lesson24/misc/cmd_search_profiles.py
+++ lesson24/misc/cmd_search_profiles.py
@@ -4,21 +4,25 @@ from conan.api.conan_api import ConanAPI
 from conan.api.output import cli_out_write
 from conan.cli.command import conan_command
 
+
 def output_json(results):
     cli_out_write(json.dumps(results, indent=4))
 
+
 def output_default(results):
     for profile, contents in results.items():
-        cli_out_write(f"Ocurrence found in profile '{profile}':")
+        cli_out_write(f"Occurrence found in profile '{profile}':")
         cli_out_write(contents)
 
-@conan_command(group="Misc. Utils", formatters={"json": output_json, 
-                                                "text": output_default})
+
+@conan_command(
+    group="Misc. Utils", formatters={"json": output_json, "text": output_default}
+)
 def search_profiles(conan_api: ConanAPI, parser, *args):
     """
     Command to search for a string ocurrence inside all Conan profiles.
     """
-    parser.add_argument('query', help="Search query.")
+    parser.add_argument("query", help="Search query.")
     args = parser.parse_args(*args)
     query = args.query.lower()
     results = {}
