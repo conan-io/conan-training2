fa5e9041c7fd1444f7b854cd706b9009a6ffbc8b
Carlos Zoido <mrgalleta@gmail.com>
2025-11-03 10:42:08 +0100

    Lesson23 (#27)
    * Lesson 23: add assets
    
    * Copy from lesson 7
    
    * Added custom licenses deployer
    
    * Improved license deployer
    
    * Formatted code
    
    ---------
    
    Co-authored-by: PerseoGI <perseog@jfrog.com>

---
+++ lesson23/license_deploy.py
@@ -0,0 +1,14 @@
+from conan.tools.files import copy
+import os
+
+
+def deploy(graph, output_folder, **kwargs):
+    for name, dep in graph.root.conanfile.dependencies.items():
+        copy(
+            graph.root.conanfile,
+            "*",
+            os.path.join(str(dep.folders.package_folder), "licenses"),
+            os.path.join(
+                output_folder, "licenses", str(dep.ref.name), str(dep.ref.version)
+            ),
+        )
