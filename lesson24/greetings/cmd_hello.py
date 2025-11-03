e0cf3d43d159b26a99667a88fe7156ac7a75ccf0
Carlos Zoido <mrgalleta@gmail.com>
2025-11-03 10:45:38 +0100

    Lesson24 (#28)
    * add lesson24
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip

---
+++ lesson24/greetings/cmd_hello.py
@@ -0,0 +1,24 @@
+import json
+
+from conan.api.conan_api import ConanAPI
+from conan.api.output import cli_out_write
+from conan.cli.command import conan_command
+
+
+def output_json(msg):
+    cli_out_write(json.dumps(msg, indent=4))
+
+
+def output_default(msg):
+    cli_out_write(msg.get("greet", "No message provided"))
+
+
+@conan_command(group="Custom", formatters={"json": output_json, "text": output_default})
+def hello(conan_api: ConanAPI, parser, *args):
+    """
+    Simple command to print a message in stdout.
+    """
+    parser.add_argument("message", help="Message to print to stdout")
+    args = parser.parse_args(*args)
+
+    return {"greet": args.message}
---
+++ lesson24/profiles/cmd_search.py
@@ -0,0 +1,34 @@
+import json
+
+from conan.api.conan_api import ConanAPI
+from conan.api.output import cli_out_write
+from conan.cli.command import conan_command
+
+
+def output_json(results):
+    cli_out_write(json.dumps(results, indent=4))
+
+
+def output_default(results):
+    for profile, contents in results.items():
+        cli_out_write(f"Occurrence found in profile '{profile}':")
+        cli_out_write(contents)
+
+
+@conan_command(
+    group="Profiles", formatters={"json": output_json, "text": output_default}
+)
+def search(conan_api: ConanAPI, parser, *args):
+    """
+    Command to search for a string ocurrence inside all Conan profiles.
+    """
+    parser.add_argument("query", help="Search query.")
+    args = parser.parse_args(*args)
+    query = args.query.lower()
+    results = {}
+    profiles = conan_api.profiles.list()
+    for profile in profiles:
+        computed_profile = str(conan_api.profiles.get_profile([profile]))
+        if query in computed_profile.lower():
+            results[profile] = computed_profile
+    return results
