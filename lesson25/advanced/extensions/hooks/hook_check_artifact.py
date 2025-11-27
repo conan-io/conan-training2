a1962706dc42ed65cca6195aa9bbc7f70befeea5
PerseoGI <perseog@jfrog.com>
2025-11-27 16:52:25 +0100

    Tweaks

--- lesson25/advanced/extensions/hooks/hook_check_artifact.py
+++ lesson25/advanced/extensions/hooks/hook_check_artifact.py
@@ -3,9 +3,9 @@ from conan.errors import ConanException
 
 def post_package(conanfile):
     package_folder = Path(conanfile.package_folder)
-    invalid_files = [p.relative_to(package_folder) for p in package_folder.glob("**/*.{pdb,pc,cmake}")]
+    invalid_files = [p.relative_to(package_folder) for p in package_folder.glob("**/*.cmake")]
     if invalid_files:
         raise ConanException(
-            "PDB, PC or CMake files found in package, "
-            "Conan recomends using 'package_info()' to define proper usage requirements.\n"
+            "CMake config files found in package, Conan recomends "
+            "using 'package_info()' to define proper usage requirements"
         )
