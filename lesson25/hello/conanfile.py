12d342eb7e95a8b8648ae8e12939badc302df2d9
PerseoGI <perseog@jfrog.com>
2025-11-28 13:30:34 +0100

    Tweaks

--- lesson25/hello/conanfile.py
+++ lesson25/hello/conanfile.py
@@ -2,17 +2,13 @@ from conan import ConanFile
 from conan.tools.cmake import CMakeToolchain, CMake, cmake_layout, CMakeDeps
 
 
-class helloRecipe(ConanFile):
+class HelloRecipe(ConanFile):
     name = "hello"
     version = "1.0"
     package_type = "library"
-
-    # Binary configuration
     settings = "os", "compiler", "build_type", "arch"
     options = {"shared": [True, False], "fPIC": [True, False]}
     default_options = {"shared": False, "fPIC": True}
-
-    # Sources are located in the same place as this recipe, copy them to the recipe
     exports_sources = "CMakeLists.txt", "src/*", "include/*", "cmake/*"
 
     def config_options(self):
