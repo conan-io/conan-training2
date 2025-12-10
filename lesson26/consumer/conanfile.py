c7d5a10da62a90a384edd8dee8aa2681f19c5080
Carlos Zoido <mrgalleta@gmail.com>
2025-12-10 17:25:39 +0100

    lesson26

---
+++ lesson26/consumer/conanfile.py
@@ -0,0 +1,21 @@
+from conan import ConanFile
+
+
+class ConsumerRecipe(ConanFile):
+    name = "consumer"
+    version = "1.0"
+
+    python_requires = "pyreq/0.1"
+    python_requires_extend = "pyreq.MyCompanyBase"
+
+    # license, author, settings and build() inherited from MyCompanyBase
+
+    def generate(self):
+        # Access module-level variables and functions
+        pyreq = self.python_requires["pyreq"].module
+        self.output.info(f"myvar: {pyreq.myvar}")
+        self.output.info(f"myfunct(): {pyreq.myfunct()}")
+
+        # Access inherited attributes from MyCompanyBase
+        self.output.info(f"license: {self.license}")
+        self.output.info(f"author: {self.author}")
